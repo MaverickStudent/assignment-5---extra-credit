a649129a97009351e6950f567293de19bd7473b7
MaverickStudent <miguelrocha@unomaha.edu>
2023-02-28 22:20:17 -0700

    Upload 3

---
+++ src/boot/firebase.js
@@ -0,0 +1,26 @@
+import firebase from "firebase/app"
+import "firebase/firestore"
+// ADD YOUR FIREBASE CONFIG OBJECT HERE:
+var firebaseConfig = {
+  apiKey: "AIzaSyDIZdHabDiROucAMqG_tZtFgy0p6z1Djhw",
+  authDomain: "movie-app-fac36.firebaseapp.com",
+  // databaseURL: "",
+  projectId: "movie-app-fac36",
+  storageBucket: "movie-app-fac36.appspot.com",
+  messagingSenderId: "644342339794",
+  appId: "1:644342339794:web:9bb4de86d87c7b268c350b"
+}
+let firebaseApp = firebase.initializeApp(firebaseConfig)
+let db = firebaseApp.firestore()
+db.enablePersistence()
+  .catch(err => {
+    if (err.code == 'failed-precondition') {
+      console.log('persistance failed')
+    } else if (err.code == 'unimplemented') {
+      console.log('persistance unvailable')
+    }
+  })
+export {db}
+
+
+
---
+++ src/pages/Error404.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="error-page">
+    <h1>404</h1>
+    <p>Oops! The page you're looking for doesn't exist.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error404',
+}
+</script>
+
+<style>
+.error-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+h1 {
+  font-size: 10rem;
+  margin: 0;
+}
+
+p {
+  font-size: 2rem;
+  margin: 2rem 0 0 0;
+}
+</style>
