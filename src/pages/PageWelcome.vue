4d5848f040048645359775d6a31d7a49f1068a5f
MaverickStudent <miguelrocha@unomaha.edu>
2023-02-28 21:30:41 -0700

    First Upload

---
+++ src/pages/PageWelcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="constrain-more q-pa-md">
+    <q-card class="card-post q-mb-md" flat bordered>
+      <q-card-section class="text-black">
+        <div class="text-h6">Welcome!</div>
+      </q-card-section>
+      <q-separator/>
+      <q-card-section class="row justify-center">
+        <q-img src="../assets/movies.jpg" style="max-width: 300px;"/>
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn flat to="/movie-list">Movies</q-btn>
+      </q-card-actions>
+      <q-separator/>
+    </q-card>
+  </q-page>
+</template>
